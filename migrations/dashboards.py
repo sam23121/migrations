229d78174ad2db5eb618287b2603f3ff66ef295f
sam23121 <smlalene@gmail.com>
2022-07-29 11:56:57 +0300

    making script automated asas possible

---
+++ migrations/dashboards.py
@@ -0,0 +1,4 @@
+from redashAPI import RedashAPIClient
+
+# Create API client instance
+Redash = RedashAPIClient(API_KEY)
--- migrations/migrate.py
+++ migrations/migrate.py
@@ -48,24 +48,38 @@ def create_mysql_connection():
     cursor = conn.cursor()
     return cursor
 
+def create_tables(cursor):                                     
+    # SQL query string
+    # sqlQuery = "CREATE TABLE IF NOT EXISTS  objects(track_id INT Primary KEY, type1 varchar(20),traveled_d REAL,avg_speed REAL, lat REAL, lon REAL, speed REAL, lon_acc REAL, lat_acc REAL, time1 REAL)"   
+    # Execute the sqlQuery
+    with open('../data/create_table.sql', 'r') as sql_file:
+        cursor.execute(sql_file.read(), multi=True)
+    # cursor.execute(sqlQuery)
+
+
 
 
 if __name__=="__main__":
-    connectionObject = pymysql.connect(host='127.0.0.1', user='root', password='Sam12345#',
-                                     db='mysql', charset="utf8mb4",cursorclass=pymysql.cursors.DictCursor)
-    
-    try:
-        # Create a cursor object
-        cursorObject = connectionObject.cursor()                                     
-        # SQL query string
-        sqlQuery = "CREATE TABLE IF NOT EXISTS  objects(track_id INT Primary KEY, type1 varchar(20),traveled_d REAL,avg_speed REAL, lat REAL, lon REAL, speed REAL, lon_acc REAL, lat_acc REAL, time1 REAL)"   
-        # Execute the sqlQuery
-        cursorObject.execute(sqlQuery)
-    except Exception as e:
-        print("Exeception occured:{}".format(e))
+    cursor = create_mysql_connection()
+    create_tables(cursor)
     for table in TABLES_LIST:
         generate_tsv(table)
         import_tsv(table)
+    
+    # connectionObject = pymysql.connect(host='127.0.0.1', user='root', password='Sam12345#',
+    #                                  db='mysql', charset="utf8mb4",cursorclass=pymysql.cursors.DictCursor)
+    
+    # try:
+    #     # Create a cursor object
+    #     cursorObject = connectionObject.cursor()                                     
+    #     # SQL query string
+    #     sqlQuery = "CREATE TABLE IF NOT EXISTS  objects(track_id INT Primary KEY, type1 varchar(20),traveled_d REAL,avg_speed REAL, lat REAL, lon REAL, speed REAL, lon_acc REAL, lat_acc REAL, time1 REAL)"   
+    #     # Execute the sqlQuery
+    #     cursorObject.execute(sqlQuery)
+    # except Exception as e:
+    #     print("Exeception occured:{}".format(e))
+
+    
 
 
 

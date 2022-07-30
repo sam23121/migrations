f1ac47a22820790d79168d56a4210499c0ac2242
sam23121 <smlalene@gmail.com>
2022-07-31 00:42:28 +0300

    fixing script

--- migrations/dashboards.py
+++ migrations/dashboards.py
@@ -3,14 +3,15 @@ from supersetapiclient.client import SupersetClient
 
 # create api client instance for superset
 client = SupersetClient(
-    host="http://localhost:8080",
-    username="sam",
-    password="sam123",
+    host="http://localhost:8088",
+    username="admin",
+    password="admin",
 )
+API_KEY = 'LwVDxKku9xEyCBPGjidONYmoDDj1Ay9isqJAEJmS'
 # Create API client instance for redash
-Redash = RedashAPIClient()
+Redash = RedashAPIClient(API_KEY)
 res = Redash.get('dashboards')
-res.json()
+print(res.json())
 
 
 # adding the json file dashboard into superset
